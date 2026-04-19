09d4fbcb408cb33594f7455ebf798956203c6a1e
Vedant Narayan <vedantnarayan50@gmail.com>
2026-04-19 16:02:50 -0400

    montecarlo thingy - flawed

--- .osmium_mc_optimizer/mc_strategy.py
+++ .osmium_mc_optimizer/mc_strategy.py
@@ -33,7 +33,7 @@ class Trader:
     
     OSMIUM_EMA_ALPHA                    =      0.070
     OSMIUM_INNER_OFFSET                 =       9
-    OSMIUM_OUTER_OFFSET                 =      11
+    OSMIUM_OUTER_OFFSET                 =      10
     OSMIUM_VOLUME_SKEW_AGGRESSION       =      1.450
     OSMIUM_OIM_SHIFT                    =       0
     OSMIUM_BASE_QUOTE_SIZE              =      19
@@ -41,7 +41,7 @@ class Trader:
     OSMIUM_KILL_SWITCH_THRESHOLD        =      75
     OSMIUM_OIM_THRESHOLD                =      0.910
     OSMIUM_OIM_FADE_SCALE               =      0.000
-    OSMIUM_OIM_EDGE_SCALE               =      1.100
+    OSMIUM_OIM_EDGE_SCALE               =      1.000
     OSMIUM_OIM_TAKE_SCALE               =      1.100
     OSMIUM_FV_TETHER_SCALE              =      0.042
 
